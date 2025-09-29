67c1c76f7e5176dc7da9e8054fece723e4d7fcf0
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2025-09-29 11:27:15 -0300

    Update main

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ import pandas as pd
 df1=pd.read_excel('./datasets/Afastamento - PBI - 29-09-25.xlsx')
 df2=pd.read_excel('./datasets/Servidores com direito a licença - Cargo e Data.xlsx')
 
+print ('-----------//------------')
+
 print(df1)
 
-print ('---------//----------')
+print ('-----------//------------')
 
 print(df2)
 
+print ('-----------//------------')
+
+print(df1.iloc[1])
+print(df2.iloc[1])
+
+
 print ('---------/-FIM-/----------')
+
